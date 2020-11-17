459b3190475983968edebb73a3d398e4d84a3cef
Cao Mai <caomai@rocketmail.com>
2020-11-16 16:49:48 -0800

    dynamic content and footer

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (  
+    <header>
+        <h3 class="Title">Mitchell Hudson copyright 2019</h3>
+    </header>
+    )
+}
+
+export default Footer;
