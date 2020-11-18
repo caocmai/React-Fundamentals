5cc4aff2e17c536a7dd08572a4e109e934f27ca8
Cao Mai <caomai@rocketmail.com>
2020-11-17 20:39:52 -0800

    update list styles

--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -8,17 +8,19 @@ function POPOSSpace(props) {
     const { name, image, address, hours, id } = props
     return (
         <div className="POPOSSpace">
+
           <Link to={`/details/${id}`}>
           <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
           </Link>
 
-          <Link to={`/details/${id}`}>
-
+          <Link className="POPOSSpace-title" to={`/details/${id}`}>
           <h1>{name}</h1>
-
           </Link>
-          <div>{address}</div>
-          <div className="hours">{hours}</div>
+
+          <div className="POPOSSpace-info">
+                <div >{address}</div>
+                <div>{hours}</div>
+            </div>
         </div>
       )
 }
