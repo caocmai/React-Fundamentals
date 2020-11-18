c70b631a7cd8e052b718918563ef6ccabebd0618
Cao Mai <caomai@rocketmail.com>
2020-11-17 21:32:46 -0800

    add amenities to spaces

--- src/components/POPOSDetails/POPOSDetails.js
+++ src/components/POPOSDetails/POPOSDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import data from '../../sfpopos-data.json';
 import './POPOSDetails.css';
+import POPOSFeatureList from '../POPOSFeatureList/POPOSFeatureList'
 
 function POPOSDetails(props) {
   const { id } = props.match.params // Location index
@@ -15,7 +16,7 @@ function POPOSDetails(props) {
         <h1 className="POPOSDetails-title">{ title }</h1>
         <p className="POPOSDetails-desc">{ desc }</p>
         <p className="POPOSDetails-hours">{ hours }</p>
-        <p className="POPOSDetails-features">{ features }</p>
+        <POPOSFeatureList features={features}/>
         <p className="POPOSDetails-geo">{ geo.lat } { geo.lon }</p>
       </div>
     </div>
