1e14442f321941ef8418da1ed1f39b715b594a9c
Cao Mai <caomai@rocketmail.com>
2020-11-17 21:02:33 -0800

    add search function

--- src/components/POPOSList/POPOSList.js
+++ src/components/POPOSList/POPOSList.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import POPOSSpace from "../POPOSSpace/POPOSSpace";
 import "./POPOSList.css";
 import data from "../../sfpopos-data.json";
 
 function POPOSList() {
-  const spaces = data.map(({ title, address, images, hours }, i) => {
+  const [ query, setQuery ] = useState('')
+  const spaces = data.filter((obj) => {
+    // true if query is in title
+    const inTitle = obj.title.toLowerCase().includes(query.toLowerCase())
+    // true if query is in address
+    const inAddress = obj.address.toLowerCase().includes(query.toLowerCase())
+    // return true if either is true
+    return inTitle || inAddress
+}).map(({ title, address, images, hours }, i) => {
     return (
       <POPOSSpace
         id = {i}
@@ -17,7 +25,19 @@ function POPOSList() {
     );
   });
 
-  return <div className='POPOSList'>{spaces}</div>;
+  return (
+    <div className="POPOSList">
+            <form>
+                <input 
+                    value={query}
+                    placeholder="search"
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button type="submit">Submit</button>
+            </form>
+            {spaces}
+    </div>
+    )
 }
 
 export default POPOSList;
