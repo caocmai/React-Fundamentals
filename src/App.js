550b1ac81ca0eb35c37ca008feae2440f2fd0cf5
Cao Mai <caomai@rocketmail.com>
2020-11-17 20:18:53 -0800

    reorganize files and folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-import Title from './Title';
-import POPOSList from './POPOSList';
-import Footer from './Footer';
+import Title from './components/Title/Title';
+import POPOSList from './components/POPOSList/POPOSList';
+import Footer from './components/Footer/Footer';
 import { HashRouter as Router, Route } from 'react-router-dom';
-import About from './About';
-import POPOSDetails from './POPOSDetails';
+import About from './components/About/About';
+import POPOSDetails from './components/POPOSDetails/POPOSDetails';
 
 
 
