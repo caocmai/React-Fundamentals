4d7e6457cb551b4383390cd5000ae8065b6aea3b
Cao Mai <caomai@rocketmail.com>
2020-12-02 15:35:19 -0800

    [add] slash to image path

--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -10,7 +10,7 @@ function POPOSSpace(props) {
         <div className="POPOSSpace">
 
           <Link to={`/details/${id}`}>
-          <img src={`${process.env.PUBLIC_URL}images/${image}`} width="100%"alt="space_pic" />
+          <img src={`${process.env.PUBLIC_URL}/images/${image}`} width="100%"alt="space_pic" />
           </Link>
 
           <Link className="POPOSSpace-title" to={`/details/${id}`}>
